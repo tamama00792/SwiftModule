4541c82c16591acc2ad17a34ae06f3ce3e26ea6d
satori's mini <tamama00792@sina.com>
2024-03-15 17:27:25 +0800

    [功能]添加注释、增加访问权限

--- SwiftModule/Classes/SWMSwiftUtils.swift
+++ SwiftModule/Classes/SWMSwiftUtils.swift
@@ -7,7 +7,8 @@
 
 import Foundation
 
-class SWMSwiftUtils : NSObject {
+/// 跨组件内调用Swift方法的工具类（被调用方）（只有继承NSObject的类可以被OC调用）
+public class SWMSwiftUtils : NSObject {
     @objc public class func logClassMethod(_ info:String) {
         print("\(#function) \(info)")
     }
